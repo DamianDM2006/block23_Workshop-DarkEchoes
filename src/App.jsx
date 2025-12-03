93e3d18c8266c6cbd40a9e28e36db9f96d64b41e
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-12-02 17:56:12 -0700

    update to App.jsx: although having problems rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  // const EpisodeDetails () => {
+  //   if (!selectedEpisode) {
+  //     return (
+  //       <section className="details">
+  //         <h2>Episode Synopsis</h2>
+  //         <p>Select an episode to learn more.</p>
+  //       </section>
+  //     );
+  //   }
+  //   return (
+  //     <section className="details">
+  //       <h2>{selectedEpisode.id}</h2>
+  //       <h3>{selectedEpisode.title}</h3>
+  //     </section>
+  //   )
+  // }
+
+  const LineUp = () => {
+    return (
+      <section className="line">
+        <h2>Dark Echoes</h2>
+        
+      </section>
+    )
+  }
 }
