b941e25d436325e8fa406ababb7fb37265d58c22
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-12-02 18:17:40 -0700

    updated App.jsx: having internet issues now.  I'm missing something important.  Waiting until WiFi is up to continue. Tethering is too slow for UTube.

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,36 @@ export default function App() {
   const [episodes] = useState(episodeList);
   const [selectedEpisode, setSelectedEpisode] = useState();
 
-  // const EpisodeDetails () => {
-  //   if (!selectedEpisode) {
-  //     return (
-  //       <section className="details">
-  //         <h2>Episode Synopsis</h2>
-  //         <p>Select an episode to learn more.</p>
-  //       </section>
-  //     );
-  //   }
-  //   return (
-  //     <section className="details">
-  //       <h2>{selectedEpisode.id}</h2>
-  //       <h3>{selectedEpisode.title}</h3>
-  //     </section>
-  //   )
-  // }
+  const EpisodeDetails () => {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Synopsis</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.id}</h2>
+        <h3>{selectedEpisode.title}</h3>
+      </section>
+    );
+  }
 
   const LineUp = () => {
     return (
       <section className="line">
         <h2>Dark Echoes</h2>
-        
+        <ul className="line">
+          {episodes.map ((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+
       </section>
-    )
+    );
   }
 }
